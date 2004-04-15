1a375777ac629183a40fbbcc9037ed1c5cab56b1
sandrorrfreire <sandrorrfreire@66345640-f411-406e-acba-706ed0d45c9c>
2004-04-15 12:34:19 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@110 66345640-f411-406e-acba-706ed0d45c9c

--- make_b32.bat
+++ make_b32.bat
@@ -1,8 +1,8 @@
 @echo off
 if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
-if "%HB_PATH%" ==xharbour goto FLAG
-if "%HB_PATH%" ==XHARBOUR goto FLAG
+if "%HB_PATH%" ==c:\xharbour goto FLAG
+if "%HB_PATH%" ==C:\XHARBOUR goto FLAG
 if not exist lib md lib
 if not exist obj md obj
 

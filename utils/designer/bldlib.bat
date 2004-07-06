dd6803faeb77088d2da9ce50a97aa90466859dcc
alkresin <alkresin@66345640-f411-406e-acba-706ed0d45c9c>
2004-07-06 06:27:01 +0000

    2004-07-06 10:30 UTC+0300 Alexander Kresin <alex@belacy.belgorod.su>
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@259 66345640-f411-406e-acba-706ed0d45c9c

--- utils/designer/bldlib.bat
+++ utils/designer/bldlib.bat
@@ -3,14 +3,13 @@
 set HRB_DIR=%HB_Path%
 set HWGUI_INSTALL=..\..
 
-%HRB_DIR%\bin\harbour main.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
 %HRB_DIR%\bin\harbour designer.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include -DINTEGRATED -DMODAL
 %HRB_DIR%\bin\harbour hctrl.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
 %HRB_DIR%\bin\harbour hformgen.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
 %HRB_DIR%\bin\harbour inspect.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
 %HRB_DIR%\bin\harbour editor.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
 
-   bcc32 -c -O2 -tW -M -I%HRB_DIR%\include main.c designer.c hctrl.c hformgen.c inspect.c editor.c
+   bcc32 -c -O2 -tW -M -I%HRB_DIR%\include designer.c hctrl.c hformgen.c inspect.c editor.c
    del %HWGUI_INSTALL%\lib\designer.lib
    tlib %HWGUI_INSTALL%\lib\designer +designer +hctrl +hformgen +inspect +editor
 

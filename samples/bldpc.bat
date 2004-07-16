fe3c40b843504047982b71e6b58c99f617cc28ec
sandrorrfreire <sandrorrfreire@66345640-f411-406e-acba-706ed0d45c9c>
2004-07-16 14:04:10 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@268 66345640-f411-406e-acba-706ed0d45c9c

--- samples/bldpc.bat
+++ samples/bldpc.bat
@@ -1,3 +1,7 @@
+Rem
+Rem File to compiler using Pelles C
+Rem
+
 echo off
 @echo.Building file %1.prg for Pelles C Compiler  
 ECHO %1.obj > make.tmp 
@@ -37,15 +41,23 @@ ECHO %POCC%\LIB\gdi32.lib >> make.tmp
 ECHO %POCC%\LIB\winspool.lib >> make.tmp
 ECHO %POCC%\LIB\uuid.lib >> make.tmp
 ECHO %POCC%\LIB\portio.lib >> make.tmp
+IF EXIST obj\%1.res echo obj\%1.res  >> make.tmp
+echo obj\hwgui_xp.res >> make.tmp
 
-%HRB_DIR%\BIN\HARBOUR %1.prg -i%POCC%\INCLUDE;%HRB_DIR%\INCLUDE;%HWGUI_INSTALL%\INCLUDE -n -q0 -w -es2 -gc0
 
-%POCC%\bin\pocc %1.c /Ze /D"NEED_DUMMY_RETURN" /D"__XCC__" /I"INCLUDE" /I"%HRB_DIR%\INCLUDE" /I"%POCC%\INCLUDE" /I"%POCC%\INCLUDE\WIN" /I"%POCC%\INCLUDE\MSVC" /D"HB_STATIC_STARTUP" /c 
+rem %HRB_DIR%\BIN\HARBOUR %1.prg -i%POCC%\INCLUDE;%HRB_DIR%\INCLUDE;%HWGUI_INSTALL%\INCLUDE -n -q0 -w -es2 -gc0
+%HRB_DIR%\BIN\HARBOUR %1.prg -i%POCC%\INCLUDE;%HRB_DIR%\INCLUDE;%HWGUI_INSTALL%\INCLUDE -n -q0 -es2 -gc0
+
+IF EXIST %1.rc %POCC%\BIN\porc -r %1 -foobj\%1
+echo 1 24 "..\image\WindowsXP.Manifest" > obj\hwgui_xp.rc
+%POCC%\BIN\porc -r obj\hwgui_xp -foobj\hwgui_xp
+
+%POCC%\bin\pocc %1.c /Ze /D"NEED_DUMMY_RETURN" /D"__XCC__" /I"INCLUDE" /I"%HRB_DIR%\INCLUDE" /I"%POCC%\INCLUDE" /I"%POCC%\INCLUDE\WIN" /I"%POCC%\INCLUDE\MSVC" /D"HB_STATIC_STARTUP" /c
 
 %POCC%\bin\POLINK /LIBPATH:%POCC%\lib /OUT:%1.EXE /MACHINE:IX86 /OPT:WIN98 /SUBSYSTEM:CONSOLE /FORCE:MULTIPLE @make.tmp >error.log
 Echo.Building Complete
 DEL make.tmp
 del %1.c
-del %1.map
+rem del %1.map
 del %1.obj
 

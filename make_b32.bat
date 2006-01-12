0be2bb562fea5ba407e810f9948033e5e9862d79
lculik <lculik@66345640-f411-406e-acba-706ed0d45c9c>
2006-01-12 10:19:32 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@524 66345640-f411-406e-acba-706ed0d45c9c

--- make_b32.bat
+++ make_b32.bat
@@ -4,12 +4,17 @@ if "%1" == "CLEAN" goto CLEAN
 
 if not exist lib md lib
 if not exist obj md obj
-
-
+if not exist obj\b32 md obj\b32
+if not exist obj\b32\mt md obj\b32\mt
 :BUILD
 
-   make -fmakefile.bc  > make_b32.log
-   if errorlevel 1 goto BUILD_ERR
+rem   make -fmakefile.bc  > make_b32.log
+rem   if errorlevel 1 goto BUILD_ERR
+make -l EXE_OBJ_DIR=obj\b32\bin OBJ_DIR=obj\b32 -fmakefile.bc %1 %2 %3 > make_b32.log
+if errorlevel 1 goto BUILD_ERR
+make -l OBJ_DIR=obj\b32\mt -DHB_THREAD_SUPPORT -DHB_MT=mt -fmakefile.bc %2 %3 >> make_b32.log
+if errorlevel 1 goto BUILD_ERR
+
 
 :BUILD_OK
 
@@ -23,8 +28,11 @@ if not exist obj md obj
 :CLEAN
    del lib\*.lib
    del lib\*.bak
-   del obj\*.obj
-   del obj\*.c
+   del obj\b32\*.obj
+   del obj\b32\*.c
+   del obj\b32\mt\*.obj
+   del obj\b32\mt\*.c
+
    del make_b32.log
 
    goto EXIT

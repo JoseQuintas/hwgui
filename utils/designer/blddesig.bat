8d470963a64cd4831c756c5b66f70e8de09859cb
lculik <lculik@66345640-f411-406e-acba-706ed0d45c9c>
2007-06-05 23:33:36 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@732 66345640-f411-406e-acba-706ed0d45c9c

--- utils/designer/blddesig.bat
+++ utils/designer/blddesig.bat
@@ -20,7 +20,7 @@ echo inspect.obj + >> b32.bc
 echo editor.obj, + >> b32.bc
 echo designer.exe, + >> b32.bc
 echo designer.map, + >> b32.bc
-echo %HRB_DIR%\lib\richgui.lib + >> b32.bc
+
 echo %HWGUI_INSTALL%\lib\hwgui.lib + >> b32.bc
 echo %HWGUI_INSTALL%\lib\procmisc.lib + >> b32.bc
 echo %HWGUI_INSTALL%\lib\hbxml.lib + >> b32.bc

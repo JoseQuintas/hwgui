ae73216aab70eb3a9787308e953138a950f531c4
andijahja <andijahja@66345640-f411-406e-acba-706ed0d45c9c>
2009-12-20 00:03:16 +0000

    2009-12-20 07:00 UTC+0700 Andi Jahja <xharbour/AT/telkom/net/id>    * source/draw.c    * source/printdos.prg      ! hb_fopen -> fopen, reverted since there are many users still using        old version of (x)Harbour which does not yet this function.
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@1429 66345640-f411-406e-acba-706ed0d45c9c

--- source/draw.c
+++ source/draw.c
@@ -1,5 +1,5 @@
 /*
- * $Id: draw.c,v 1.61 2009-12-18 04:36:38 andijahja Exp $
+ * $Id: draw.c,v 1.62 2009-12-20 00:03:16 andijahja Exp $
  *
  * HWGUI - Harbour Win32 GUI library source code:
  * C level painting functions
@@ -837,7 +837,7 @@ HB_FUNC( OPENIMAGE )
    }
    else
    {
-      fp = hb_fopen( cFileName, "rb" );
+      fp = fopen( cFileName, "rb" );
       if( !fp )
       {
          HB_RETHANDLE( 0 );
--- source/printdos.prg
+++ source/printdos.prg
@@ -1,5 +1,5 @@
 /*
- * $Id: printdos.prg,v 1.25 2009-12-18 02:26:49 andijahja Exp $
+ * $Id: printdos.prg,v 1.26 2009-12-20 00:03:16 andijahja Exp $
  *
  * CLASS PrintDos
  *
@@ -625,7 +625,7 @@ HB_FUNC( AFILLTEXT )
       hb_reta( 0 );
       return;
    }
-   inFile = hb_fopen( pSrc, "r" );
+   inFile = fopen( pSrc, "r" );
 
    if ( !inFile )
    {

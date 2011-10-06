ffdd7fc4f7c1d1940c2809c40afc83c83a6a17ed
LFBASSO <LFBASSO@66345640-f411-406e-acba-706ed0d45c9c>
2011-10-06 14:44:51 +0000

    2011-10-06 11:50 UTC-0300 Luis Fernando Basso <lfbasso at via-rs.net>    + file make_b32pointer.bat      compiled with FLAGS DHWG_USE_POINTER_ITEM
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@1757 66345640-f411-406e-acba-706ed0d45c9c

---
+++ make_b32pointer.bat
@@ -0,0 +1,42 @@
+@echo off
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
+
+if not exist lib md lib
+if not exist obj md obj
+if not exist obj\b32 md obj\b32
+if not exist obj\b32\mt md obj\b32\mt
+:BUILD
+
+rem   make -fmakefile.bc  > make_b32.log
+rem   if errorlevel 1 goto BUILD_ERR
+rem   set ACTIVEX_SUPPORT=ON
+make -l EXE_OBJ_DIR=obj\b32\bin OBJ_DIR=obj\b32 -fmakefilepointer.bc %1 %2 %3 > make_b32.log
+if errorlevel 1 goto BUILD_ERR
+make -l OBJ_DIR=obj\b32\mt -DHB_THREAD_SUPPORT -DHB_MT=mt -fmakefilepointer.bc %2 %3 >> make_b32.log
+if errorlevel 1 goto BUILD_ERR
+
+
+:BUILD_OK
+
+   goto EXIT
+
+:BUILD_ERR
+
+   notepad make_b32.log
+   goto EXIT
+
+:CLEAN
+   del lib\*.lib
+   del lib\*.bak
+   del obj\b32\*.obj
+   del obj\b32\*.c
+   del obj\b32\mt\*.obj
+   del obj\b32\mt\*.c
+
+   del make_b32.log
+
+   goto EXIT
+
+:EXIT
+

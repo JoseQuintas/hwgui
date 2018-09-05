d409465e20200795f104543ccfa305963722cc41
alkresin <alkresin@66345640-f411-406e-acba-706ed0d45c9c>
2018-09-05 14:02:44 +0000

    2018-09-05 17:04 UTC+0300 Alexander Kresin <alex@belacy.ru>   * whatsnew.txt     * Updated with a short info about a new build.   * utils/tutorial/bldgw.bat     ! Small fix
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@2661 66345640-f411-406e-acba-706ed0d45c9c

--- utils/tutorial/bldgw.bat
+++ utils/tutorial/bldgw.bat
@@ -1,11 +1,10 @@
 @echo off
-rem set HB_PATH=c:\harbour_v3
 rem set path=c:\softools\mingw\bin;%HB_PATH%\bin
 set HRB_DIR=%HB_PATH%
 set HRB_LIB_DIR=%HB_PATH%\lib\win\mingw
 set HWGUI_INSTALL=..\..
 set HWGUI_LIBS=-lhwgui -lprocmisc -lhbxml
-if exist %HRB_LIB_DIR%\libhbvm.lib goto hrb
+if exist %HRB_LIB_DIR%\libhbvm.a goto hrb
 goto common
 set HRB_LIBS=-lvm -lrdd -lmacro -lpp -lrtl -lcodepage -llang -lcommon -ldbfntx  -ldbfcdx -ldbffpt -lhsx -lhbsix -lgtgui -lgtwin
 :hrb
@@ -17,11 +16,11 @@ echo 1 24 "../../image/WindowsXP.Manifest" > hwgui_xp.rc
 
 gcc -I. -I%HRB_DIR%\include -Wall -c tutor.c -otutor.o
 windres hwgui_xp.rc hwgui_xp.o
-gcc -Wall -mwindows -otutor.exe tutor.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -mno-cygwin -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
+gcc -Wall -mwindows -otutor.exe tutor.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
 
 %HRB_DIR%\bin\harbour hwgrun.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2
 gcc -I. -I%HRB_DIR%\include -Wall -c hwgrun.c -ohwgrun.o
-gcc -Wall -mwindows -ohwgrun.exe hwgrun.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -mno-cygwin -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
+gcc -Wall -mwindows -ohwgrun.exe hwgrun.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
 
 del *.c
 del *.o

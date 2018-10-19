f5bf8f37b24d3dbea4e1b171f17201e7ff3b3332
alkresin <alkresin@66345640-f411-406e-acba-706ed0d45c9c>
2018-10-19 09:50:35 +0000

    2018-10-19 12:50 UTC+0300 Alexander Kresin <alex@belacy.ru>   * utils/tutorial/bld.bat   * utils/designer/blddesig.bat     ! Fixes
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@2674 66345640-f411-406e-acba-706ed0d45c9c

--- utils/designer/blddesig.bat
+++ utils/designer/blddesig.bat
@@ -2,7 +2,7 @@
 
 rem set HB_PATH=c:\harbour_v3
 set HRB_DIR=%HB_Path%
-set HRB_LIBS=%HRB_DIR%\lib
+set HRB_LIBS=%HRB_DIR%\lib\win\bcc
 set HWGUI_INSTALL=..\..
 
 %HRB_DIR%\bin\harbour designer.prg -w -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include
--- utils/tutorial/bld.bat
+++ utils/tutorial/bld.bat
@@ -4,7 +4,7 @@ set HRB_DIR=%HB_PATH%
 set HWGUI_INSTALL=..\..
 set HWGUI_LIBS=hwgui.lib hbxml.lib procmisc.lib
 
-if exist %HRB_DIR%\lib\hbvm.lib goto hrb
+if exist %HRB_DIR%\lib\win\bcc\hbvm.lib goto hrb
 set HRB_LIBS=vm.lib rtl.lib gtgui.lib gtwin.lib codepage.lib lang.lib rdd.lib macro.lib pp.lib dbfntx.lib dbfcdx.lib dbffpt.lib hsx.lib hbsix.lib common.lib ct.lib
 goto common
 :hrb
@@ -17,12 +17,12 @@ echo 1 24 "..\..\image\WindowsXP.Manifest" > hwgui_xp.rc
 brc32 -r hwgui_xp -fohwgui_xp
 
 bcc32 -c -O2 -tW -M -I%HRB_DIR%\include tutor.c  
-ilink32 -Gn -aa -Tpe -L%HRB_DIR%\lib;%HWGUI_INSTALL%\lib c0w32.obj tutor.obj, tutor.exe, tutor.map, %HWGUI_LIBS% %HRB_LIBS% ws2_32.lib cw32.lib import32.lib,, hwgui_xp.res
+ilink32 -Gn -aa -Tpe -L%HRB_DIR%\lib\win\bcc;%HWGUI_INSTALL%\lib c0w32.obj tutor.obj, tutor.exe, tutor.map, %HWGUI_LIBS% %HRB_LIBS% ws2_32.lib cw32.lib import32.lib,, hwgui_xp.res
 
 %HRB_DIR%\bin\harbour hwgrun.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2 %3
 
 bcc32 -c -O2 -tW -M -I%HRB_DIR%\include hwgrun.c
-ilink32 -Gn -aa -Tpe -L%HRB_DIR%\lib;%HWGUI_INSTALL%\lib c0w32.obj hwgrun.obj, hwgrun.exe, hwgrun.map, %HWGUI_LIBS% %HRB_LIBS% ws2_32.lib cw32.lib import32.lib,, hwgui_xp.res
+ilink32 -Gn -aa -Tpe -L%HRB_DIR%\lib\win\bcc;%HWGUI_INSTALL%\lib c0w32.obj hwgrun.obj, hwgrun.exe, hwgrun.map, %HWGUI_LIBS% %HRB_LIBS% ws2_32.lib cw32.lib import32.lib,, hwgui_xp.res
 
 del *.obj
 del *.c

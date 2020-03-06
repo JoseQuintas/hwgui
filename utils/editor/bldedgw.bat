0badb815c0e6a61a665a723c770245f418025416
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2020-03-06 14:50:59 +0000

    Tickets #15,18,19,20:Missing build scripts for MingW, added
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@2807 66345640-f411-406e-acba-706ed0d45c9c

---
+++ utils/editor/bldedgw.bat
@@ -0,0 +1,29 @@
+@echo off
+REM build HWGUI editor for MINGW (gcc) on Windows
+REM $Id$
+REM by DF7BE
+REM 2019-09-09
+
+rem configure installation path of Harbour and gcc to your own needs 
+rem set HB_PATH=c:\harbour_v3
+REM set HRB_DIR=%HB_Path%
+SET HRB_DIR=C:\harbour\core-master
+REM
+set HRB_LIBS=%HRB_DIR%\lib\win\mingw
+set HRB_EXE=%HRB_DIR%\bin\win\mingw\harbour.exe
+REM configure HWGUI install (current dir is utils\designer )
+set HWGUI_INSTALL=..\..
+
+
+
+REM   gcc -I. -I%HRB_DIR%\include -Wall -c  hctrl.c -o  hctrl.o
+   
+REM compling resource file
+REM echo 1 24 "..\..\image\WindowsXP.Manifest" > hwgui_xp.rc
+REM no effect visible
+
+REM and link
+hbmk2 editor -I%HWGUI_INSTALL%\include -L%HWGUI_INSTALL%\lib -lhwgui -lprocmisc -lhbxml -lhwgdebug -gui
+
+
+REM ======= EOF of bldedgw.bat =========

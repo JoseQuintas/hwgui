7ab988e2407a8e7053009c889e3fef2ad7023d9a
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2020-03-06 16:31:40 +0000

    Ticket #21 tutorial: HRB_EXE added
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@2810 66345640-f411-406e-acba-706ed0d45c9c

--- utils/tutorial/bldgw.bat
+++ utils/tutorial/bldgw.bat
@@ -2,6 +2,8 @@
 rem set path=c:\softools\mingw\bin;%HB_PATH%\bin
 set HRB_DIR=%HB_PATH%
 set HRB_LIB_DIR=%HB_PATH%\lib\win\mingw
+REM configure Path of Harbour exe to your own needs
+set HRB_EXE=%HB_PATH%\bin\win\mingw\harbour.exe
 set HWGUI_INSTALL=..\..
 set HWGUI_LIBS=-lhwgui -lprocmisc -lhbxml
 if exist %HRB_LIB_DIR%\libhbvm.a goto hrb
@@ -11,14 +13,14 @@ set HRB_LIBS=-lvm -lrdd -lmacro -lpp -lrtl -lcodepage -llang -lcommon -ldbfntx  
 set HRB_LIBS=-lhbvm -lhbrdd -lhbmacro -lhbpp -lhbrtl -lhbcpage -lhblang -lhbcommon -lrddntx  -lrddcdx -lrddfpt -lhbsix -lgtgui -lgtwin -lhbcplr
 :common
 
-%HRB_DIR%\bin\harbour tutor.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2
+%HRB_EXE% tutor.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2
 echo 1 24 "../../image/WindowsXP.Manifest" > hwgui_xp.rc
 
 gcc -I. -I%HRB_DIR%\include -Wall -c tutor.c -otutor.o
 windres hwgui_xp.rc hwgui_xp.o
 gcc -Wall -mwindows -otutor.exe tutor.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
 
-%HRB_DIR%\bin\harbour hwgrun.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2
+%HRB_EXE% hwgrun.prg -n -i%HRB_DIR%\include;%HWGUI_INSTALL%\include %2
 gcc -I. -I%HRB_DIR%\include -Wall -c hwgrun.c -ohwgrun.o
 gcc -Wall -mwindows -ohwgrun.exe hwgrun.o hwgui_xp.o -L%HRB_LIB_DIR% -L%HWGUI_INSTALL%\lib -Wl,--allow-multiple-definition -Wl,--start-group %HWGUI_LIBS% %HRB_LIBS% -luser32 -lwinspool -lcomctl32 -lcomdlg32 -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -Wl,--end-group
 

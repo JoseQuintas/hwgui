aadd66cfe4a5f556031583e2868626f313d418f3
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2022-11-23 10:42:10 +0000

    PATH to Msys2 system programs corrected
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3152 66345640-f411-406e-acba-706ed0d45c9c

--- samples/dev/env/pfad_msys.bat
+++ samples/dev/env/pfad_msys.bat
@@ -5,5 +5,5 @@ REM
 REM Path to HWHUI is C:\Msys64\hwgui\hwgui
 SET HB_PATH=C:\Msys64\harbour\core-master
 SET HRB_EXE=%HB_PATH%\bin\win\mingw64\harbour.exe
-SET PATH=%PATH%;C:\Msys64\bin;C:\Msys64\harbour\core-master\bin\win\mingw64;C:\Msys64\make\bin;C:\Msys64\cmake\bin;C:\Msys64\hwgui\hwgui\bin 
+SET PATH=%PATH%;C:\Msys64\usr\bin;C:\Msys64\harbour\core-master\bin\win\mingw64;C:\Msys64\make\bin;C:\Msys64\cmake\bin 
 REM =========== EOF of pfad_msys.bat ===========

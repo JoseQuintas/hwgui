8d05fd3b6a6cd7390c773ed0d0cd6d77d8d134ad
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2022-11-25 10:52:02 +0000

    Project Port to GTK3 started (on Msys2)
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3154 66345640-f411-406e-acba-706ed0d45c9c

---
+++ include/gtk3missing.h
@@ -0,0 +1,24 @@
+/*
+ *
+ *  gtk3missing.h
+ * 
+ * $Id$
+ *
+ * This include file sets definition of 
+ * type "uid_t"
+ * for compilation of GTK3 on Msys2
+ * (Cross development environment on Windows)
+ *
+*/
+
+#ifndef uid_t
+#define uid_t int  /* UID type */
+#endif
+
+#ifndef Uid_t_size
+#define Uid_t_size 4  /* UID size */
+#endif
+
+
+
+/* ================== EOF of gtk3missing.h ======================== */
--- samples/dev/env/pfad_msys.bat
+++ samples/dev/env/pfad_msys.bat
@@ -5,5 +5,7 @@ REM
 REM Path to HWHUI is C:\Msys64\hwgui\hwgui
 SET HB_PATH=C:\Msys64\harbour\core-master
 SET HRB_EXE=%HB_PATH%\bin\win\mingw64\harbour.exe
-SET PATH=%PATH%;C:\Msys64\usr\bin;C:\Msys64\harbour\core-master\bin\win\mingw64;C:\Msys64\make\bin;C:\Msys64\cmake\bin 
+SET PATH=%PATH%;C:\msys64\mingw64\bin;C:\Msys64\usr\bin;C:\Msys64\harbour\core-master\bin\win\mingw64;C:\Msys64\make\bin;C:\Msys64\cmake\bin
+REM GTK 3
+SET PKG_CONFIG_PATH="C:\msys64\mingw64\lib\pkgconfig;C:\Msys64\usr\lib\pkgconfig;C:\Msys64\usr\share\pkgconfig;C:\Msys64\lib\pkgconfig"
 REM =========== EOF of pfad_msys.bat ===========
